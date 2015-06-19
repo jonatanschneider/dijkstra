b96ef1ad27e6d16ee462f96de85ad3251c0c5db6
Jonatan Schneider <j.schneider1004@gmail.com>
2015-06-19 18:20:44 +0200

    Fixed Dictionary, now StackOverflow Error

--- lib.cs
+++ lib.cs
@@ -15,7 +15,7 @@ namespace Dijkstra
 
         public static Dictionary<string, int> unvisitedWays = new Dictionary<string, int>
         {
-           /* {"AB", 6}, {"AD", 7}, {"BC", 4},
+           /*{"AB", 6}, {"AD", 7}, {"BC", 4},
             {"BE", 4}, {"CF", 6}, {"CI", 8},
             {"DG", 5}, {"EJ", 5}, {"FK", 5},
             {"GH", 5}, {"GI", 7}, {"HL", 5},
@@ -25,7 +25,8 @@ namespace Dijkstra
             {"PU", 2}, {"QX", 7}, {"RT", 8},
             {"RY", 10},{"SW", 2}, {"SX", 4},
             {"WY", 9}, {"XY", 11} */
-           //Neue Werte 
+           //Neue Werte
+           
             {"AB", 6}, {"AD", 7}, 
             {"BC", 4}, {"BE", 4}, {"BA", 6},
             {"CF", 6}, {"CI", 8}, {"CB", 4},
@@ -37,7 +38,7 @@ namespace Dijkstra
             {"IM", 5}, {"IG", 7}, {"IC", 6},
             {"JK", 7}, {"JT", 4}, {"JE", 5},
             {"KU", 9}, {"KJ", 7}, {"KF", 5},
-            {"LM", 6}, {"LM", 6},
+            {"LM", 6}, {"LH", 5},
             {"MN", 5}, {"MI", 5}, {"ML", 6},
             {"NO", 7}, {"NM", 5},
             {"OQ", 5}, {"ON", 7},
@@ -47,13 +48,13 @@ namespace Dijkstra
             {"SW", 2}, {"SX", 4},
             {"WY", 9}, {"WS", 2},
             {"XY", 11},{"XS", 4}, {"XQ", 7},
-            {"ZR", 4}, {"VT", 7} 
-        };
+            {"ZR", 4}, {"VT", 7}
+        }; 
 
         public static Dictionary<string, int> waysAndValues = new Dictionary<string, int>
         {
             
-           /* {"AB", 6}, {"AD", 7}, {"BC", 4},
+           /*{"AB", 6}, {"AD", 7}, {"BC", 4},
              {"BE", 4}, {"CF", 6}, {"CI", 8},
              {"DG", 5}, {"EJ", 5}, {"FK", 5},
              {"GH", 5}, {"GI", 7}, {"HL", 5},
@@ -64,6 +65,7 @@ namespace Dijkstra
              {"RY", 10},{"SW", 2},  {"SX", 4},
              {"WY", 9}, {"XY", 11} */
            // Neue Werte
+           
              {"AB", 6}, {"AD", 7}, 
              {"BC", 4}, {"BE", 4}, {"BA", 6},
              {"CF", 6}, {"CI", 8}, {"CB", 4},
@@ -75,7 +77,7 @@ namespace Dijkstra
              {"IM", 5}, {"IG", 7}, {"IC", 6},
              {"JK", 7}, {"JT", 4}, {"JE", 5},
              {"KU", 9}, {"KJ", 7}, {"KF", 5},
-             {"LM", 6}, {"LM", 6},
+             {"LM", 6}, {"LH", 5},
              {"MN", 5}, {"MI", 5}, {"ML", 6},
              {"NO", 7}, {"NM", 5},
              {"OQ", 5}, {"ON", 7},
